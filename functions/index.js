c162a4ab06a5c94d7f5ebc204698a3cafbe4dee6
loveisgala <gabriela.gk@hotmail.com>
2021-04-19 15:41:13 -0300

    link firebase project

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,4 @@
 const functions = require("firebase-functions");
-const Filter = require('bad-words');
-
-const admin = require('firebase-admin');
-admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
